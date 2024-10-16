391d65087a95a4238fc59ce664d68061f6de698a
Alexander Koskovich <akoskovich@pm.me>
2024-10-15 23:14:58 -0400

    Use an array for extra flags on startup

--- slack.sh
+++ slack.sh
@@ -4,7 +4,7 @@ WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
 
 if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
 then
-    EXTRA_ARGS="--enable-wayland-ime --ozone-platform-hint=auto"
+    EXTRA_FLAGS+=("--enable-wayland-ime" "--ozone-platform-hint=auto")
 fi
 
-env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s ${EXTRA_ARGS} "$@"
+env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s "${EXTRA_FLAGS[@]}" "$@"
